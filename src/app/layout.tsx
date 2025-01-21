bb0904fccd0b7904765b34d4dfe1830617ae1e64
3uxeca <sajihye4@gmail.com>
2025-01-21 16:32:03 +0900

    Chore: font 및 svgr 관련 셋팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import LayoutRecoil from "./layout.recoil";
+import localFont from 'next/font/local';
 
 export const metadata = {
   title: "myfair front pre-course",
   description: "todolist",
 };
+
+const pretendard = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Pretendard/Pretendard-Bold.subset.woff2',
+      weight: '700',
+    },
+    {
+      path: '../../public/fonts/Pretendard/Pretendard-SemiBold.subset.woff2',
+      weight: '600',
+    },
+    {
+      path: '../../public/fonts/Pretendard/Pretendard-Medium.subset.woff2',
+      weight: '500',
+    },
+    {
+      path: '../../public/fonts/Pretendard/Pretendard-Regular.subset.woff2',
+      weight: '400',
+    },
+  ],
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +35,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={pretendard.className}>
         <LayoutRecoil>{children}</LayoutRecoil>
       </body>
     </html>
