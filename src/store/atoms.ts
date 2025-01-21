aedba413639a96c8b4de989af828ccbd0e1e952c
3uxeca <sajihye4@gmail.com>
2025-01-21 18:26:53 +0900

    Chore: recoil-persist 패키지 추가 및 Todo 관련 state 정의

---
+++ src/store/atoms.ts
@@ -0,0 +1,36 @@
+import { AtomEffect, atom, useSetRecoilState } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+//Next.js에서 persistAtom을 쓰기 위한 구성
+const ssrCompletedState = atom({
+  key: "SsrCompleted",
+  default: false,
+});
+
+export const useSsrComplectedState = () => {
+  const setSsrCompleted = useSetRecoilState(ssrCompletedState);
+  return () => setSsrCompleted(true);
+};
+
+const { persistAtom } = recoilPersist();
+
+
+export const persistAtomEffect = <T>(param: Parameters<AtomEffect<T>>[0]) => {
+  param.getPromise(ssrCompletedState).then(() => persistAtom(param));
+};
+
+// atoms 시작
+
+// Todo 타입 정의
+export interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+};
+
+// todoListState 초기화
+export const todoListState = atom({
+  key: "todoListState",
+  default: [] as Todo[],
+  effects_UNSTABLE: [persistAtomEffect],
+});
