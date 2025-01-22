381c5e46ad79928c9603f43ed699cc9d52160d03
3uxeca <sajihye4@gmail.com>
2025-01-22 17:15:21 +0900

    Fix: 크롬 브라우저 input 한글 입력 후 Enter 시 데이터 중복 입력 문제

--- src/components/todo/TodoInput.tsx
+++ src/components/todo/TodoInput.tsx
@@ -30,29 +30,29 @@ const TodoInput = (props: Props) => {
   const { todoCount } = useTodoStats(todoList);  
 
   const handleAddTodo = () => {
-    if (!newTodoText) {
+    if (!newTodoText.trim()) {
       alert('할 일을 입력해 주세요.');
       return;
     }
-    // 조건 확인: 10개 이상이면 추가 금지
+  
     if (todoCount >= 10) {
       alert('할 일이 너무 많습니다. 완료 후 새로운 할 일을 추가하세요!');
       return;
     }
-
-    if (newTodoText.trim()) {
-      addTodo({ id: Date.now(), text: newTodoText, completed: false });
-      setNewTodoText('');
-    }
+  
+    // 상태 업데이트와 할 일 추가
+    addTodo({ id: Date.now(), text: newTodoText.trim(), completed: false });
+    setNewTodoText(''); // 상태 초기화
   };
 
   const onKeydown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
-      console.log(event.target, newTodoText);
+      if (event.nativeEvent.isComposing) return;  // 크롬 브라우저에서 한글을 사용하는 경우에 발생하는 2회 입력 문제 발생 방지
       handleAddTodo();  // Enter 키를 감지하고 handleAddTodo 함수 호출
     }
     return true;
   };    
+  
   return (
     <Input
       type='text'
